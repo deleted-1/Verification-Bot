3a75d3aa5f913fc78a0363403f966b279ca66d0a
gerauschlos <50221495+gerauschlos@users.noreply.github.com>
2019-08-20 23:56:01 -0400

    Handlers

---
+++ Util/index.js
@@ -0,0 +1,9 @@
+const { Client, Collection } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client();
+
+client.commands = new Collection();
+require(`../handlers/eventHandler.js`)(client);
+
+client.login(token);
