cdd2a28b12c8038e4822589beb6cbb2931b596d6
Dominic Humber <50221495+gerauschlos@users.noreply.github.com>
2019-10-03 17:23:49 -0400

    Delete index.js

--- Util/index.js
+++
@@ -1,9 +0,0 @@
-const { Client, Collection } = require('discord.js');
-const { token } = require('./config.json');
-
-const client = new Client();
-
-client.commands = new Collection();
-require(`../handlers/eventHandler.js`)(client);
-
-client.login(token);
